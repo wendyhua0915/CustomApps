8da96dee2d6ef289bca6d6492430873075ae9db8
Maxson Yang <maxsonyang@berkeley.edu>
2018-04-24 12:30:57 -0700

    Authentication is a go sort of

---
+++ CustomApp/signUpViewController.swift
@@ -0,0 +1,9 @@
+//
+//  SignUpViewController.swift
+//  CustomApp
+//
+//  Created by Maxson Yang on 4/24/18.
+//  Copyright © 2018 wendy hua. All rights reserved.
+//
+
+import Foundation
---
+++ File.swift
@@ -0,0 +1,8 @@
+//
+//  File.swift
+//  
+//
+//  Created by Maxson Yang on 4/24/18.
+//
+
+import Foundation
